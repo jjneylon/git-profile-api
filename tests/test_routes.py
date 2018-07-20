c95a06985d6eb7144a416adf327e477a1e4274ad
Joe Neylon <jjneylon@gmail.com>
2018-07-19 22:04:09 -0500

    gpa-3: added test request context decorator; added tests for routes; fixed error handling in route

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,12 +1,29 @@
-from unittest import TestCase
+from unittest import mock, TestCase
 
-from service import app
-from service.routes import get_user_profile
+from service import routes
+from tests import RequestContext
 
 
-class GetUserProfileTestCase(TestCase):
-    def test_get_user_profile(self):
-        with app.test_request_context():
-            response = get_user_profile()
+class GetProfileTestCase(TestCase):
+    @RequestContext('/api/profile?github=user1&bitbucket=user2')
+    def test_get_profile(self):
+        with mock.patch.object(routes, 'handle_get_profile', return_value={'profile': 'data'}) as mock_handler:
+            response = routes.get_profile()
 
-        self.assertEqual(response, '{}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'profile': 'data'})
+        mock_handler.assert_called_once_with('user1', 'user2')
+
+    @RequestContext('/api/profile?bitbucket=user2')
+    def test_no_github_username(self):
+        response = routes.get_profile()
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"error": "No github in request params"})
+
+    @RequestContext('/api/profile?github=user1')
+    def test_no_bitbucket_username(self):
+        response = routes.get_profile()
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {"error": "No bitbucket in request params"})
